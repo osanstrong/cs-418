4907e535b33c816a548dba968e3c93c60afe2ea7
Owen Strong <owensanstrong@gmail.com>
2026-09-05 17:48:41 -0500

    Add input file parser

--- anylang/main.cpp
+++ anylang/main.cpp
@@ -1,7 +1,87 @@
 #include "./uselibpng.h"
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <bits/stdc++.h>
 
-int main() {
+
+using StringVec = std::vector<std::string>;
+using CommandVec = std::vector<StringVec>;
+
+auto get_lines(char* const filepath) -> StringVec {
+    // Open the text file for reading
+    std::ifstream f(filepath);
+
+    // Check if the file was opened successfully
+    if (!f.is_open()) {
+        std::cerr << "Error opening the file!";
+    }
+    printf("Opening file %s\n", filepath);
+
+
+    StringVec commands;
+    std::string s;
+
+    // Read each line from the file
+    while (std::getline(f, s))
+        commands.push_back(s);
+
+    // Close the file
+    f.close();
+
+    return commands;
+}
+
+auto split(std::string target, char delim) -> StringVec {
+  StringVec tokens;
+  std::string s;
+
+  std::stringstream ss(target);
+
+  while (std::getline(ss, s, delim)) {
+    tokens.push_back(s);
+  }
+  return tokens;
+}
+
+auto get_commands(char* const filepath) -> CommandVec {
+  printf("Getting file from %s.\n", filepath);
+  CommandVec commands;
+  StringVec raw_commands = get_lines(filepath);
+  for (auto cmd : raw_commands) {
+    printf("Raw string: '%s'\n", cmd);
+    StringVec tokens = split(cmd, ' ');
+    commands.push_back(tokens);
+  }
+  return commands;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc > 1) {
+    auto filepath = argv[1];
+    // int res = print_file(filepath);
+    StringVec commands = get_lines(filepath);
+    for (auto cmd : commands) {
+      // std::cout << cmd << std::endl;
+      StringVec tokens = split(cmd, ' ');
+      printf("Command of length %lu with %lu tokens.\n", cmd.length(), tokens.size());
+    }
+  }
+
+  if (argc < 2) {
+    // No path argument
+    return 1;
+  }
+
+  auto filepath = argv[1];
+  auto commands = get_commands(filepath);
+  for (auto cmd : commands) {
+    printf("Running command '%s' with %lu args\n", cmd[0].c_str(), cmd.size()-1);
+  }
+
+
   /* ... */
   Image img = Image(500, 500);
   /* ... */
@@ -20,3 +100,4 @@ int main() {
   img.save(filename);
   printf("Image saved with dims %d by %d\n", img.width(), img.height());
 }
+
