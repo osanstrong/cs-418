4a6d5fa98462228c96956a053b2e7a079bc74099
Owen Strong <owensanstrong@gmail.com>
2026-09-06 02:26:30 -0500

    Add commands for warmup 'anylang'

--- anylang/argparse.hpp
+++ anylang/argparse.hpp
@@ -5,6 +5,7 @@
 
 
 using StringVec = std::vector<std::string>;
+using Command = StringVec;
 using CommandVec = std::vector<StringVec>;
 
 auto get_lines(char* const filepath) -> StringVec {
@@ -18,17 +19,21 @@ auto get_lines(char* const filepath) -> StringVec {
     printf("Opening file %s\n", filepath);
 
 
-    StringVec commands;
+    StringVec lines;
     std::string s;
 
     // Read each line from the file
-    while (std::getline(f, s))
-        commands.push_back(s);
+    while (std::getline(f, s, '\n')){
+      // std::cout << s << std::endl;
+      // printf("Adding string %s\n", s.c_str());
+      lines.push_back(s);
+
+    }
 
     // Close the file
     f.close();
 
-    return commands;
+    return lines;
 }
 
 auto split(std::string target, char delim) -> StringVec {
@@ -48,9 +53,21 @@ auto get_commands(char* const filepath) -> CommandVec {
   CommandVec commands;
   StringVec raw_commands = get_lines(filepath);
   for (auto cmd : raw_commands) {
-    printf("Raw string: '%s'\n", cmd);
-    StringVec tokens = split(cmd, ' ');
+    // printf("Raw string: '%s'\n", cmd.c_str());
+    Command tokens = split(cmd, ' ');
     commands.push_back(tokens);
   }
   return commands;
+}
+
+constexpr uint64_t hash(std::string_view str) {
+  uint64_t hash = 0;
+  for (char c : str) {
+    hash = (hash * 131) + c;
+  }
+  return hash;
+}
+
+constexpr uint64_t operator"" _hash(const char* str, size_t len) {
+  return hash(std::string_view(str, len));
 }
--- anylang/main.cpp
+++ anylang/main.cpp
@@ -5,8 +5,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <span>
 #include <string.h>
 #include <bits/stdc++.h>
+
+
+using Int8Vec = std::vector<uint8_t>;
+using IntVec = std::vector<int>;
 
 int main(int argc, char* argv[]) {
 
@@ -15,29 +20,84 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  auto filepath = argv[1];
-  auto commands = get_commands(filepath);
-  for (auto cmd : commands) {
-    printf("Running command '%s' with %lu args\n", cmd[0].c_str(), cmd.size()-1);
-  }
+
+  char* filepath = argv[1];
+  StringVec raw_commands = get_lines(filepath);
+  CommandVec commands = get_commands(filepath);
+  Command imgdef = commands.front(); // Assume first command is initializing the image 
+  Image img = Image(std::stoi(imgdef[1]), std::stoi(imgdef[2]));
+  auto filename = imgdef[3].c_str();
+
+  commands.erase(commands.begin());
 
 
-  /* ... */
-  Image img = Image(500, 500);
-  /* ... */
-  // int x = 0, y = 1;
-  uint8_t test_val = 255;
-  for (int x = 0; x < 100; x++) {
-    for (int y = 100; y < 200; y++) {
-      img[y][x].red = test_val;
-      img[y][x].green = test_val;
-      img[y][x].blue = test_val;
-      img[y][x].alpha = test_val;
+  // Position / color buffers
+  Int8Vec color_buffer;
+  IntVec pos_buffer;
+
+
+  for (int i = 0; i < commands.size(); i++) {
+    Command cmd = commands[i];
+    std::string raw_command = raw_commands[i];
+
+
+    // printf("Running command '%s' with %lu args\n", cmd[0].c_str(), cmd.size()-1);
+    // printf("Running command: '%s'\n", raw_command.c_str());
+    std::cout << raw_command << std::endl << "==========" << std::endl;
+    
+    std::string name = cmd[0];
+
+    switch (hash(name)) {
+      case "png"_hash:{
+        std::cerr << "command 'png' should only be used at start of file";
+        break;}
+      case "position"_hash:{
+        // Assume only position-2 is given
+        pos_buffer.clear();
+        for (int i = 2; i < cmd.size(); i+=2) {
+          pos_buffer.push_back(std::stoi(cmd[i]));
+          printf("Pushing back from x index %d\n", i);
+          pos_buffer.push_back(std::stoi(cmd[i+1]));
+          printf("Pushing back from y index %d\n", i+1);
+        }
+        break;}
+      case "color"_hash:{
+        // Assume only color-4 is given
+        color_buffer.clear();
+        for (int i = 2; i < cmd.size(); i+=4) {
+          printf("Pushing back from index %d\n", i);
+          color_buffer.push_back(std::stoi(cmd[i]));
+          printf("Pushing back from index %d\n", i+1);
+          color_buffer.push_back(std::stoi(cmd[i+1]));
+          printf("Pushing back from index %d\n", i+2);
+          color_buffer.push_back(std::stoi(cmd[i+2]));
+          printf("Pushing back from index %d\n", i+3);
+          color_buffer.push_back(std::stoi(cmd[i+3]));
+          printf("Pushed back from index %d to %d\n", i, i+3);
+        }
+        break;}
+      case "drawPixels"_hash:{
+        printf("Running draw pixel command");
+        int num_pixels = std::stoi(cmd[1]);
+        for (int i = 0; i < num_pixels; i++) {
+          printf("Pushing pixel %d of %d\n", i, num_pixels);
+          int x = pos_buffer[i*2+0], y = pos_buffer[i*2+1];
+          img[y][x].red = color_buffer[i*4+0];
+          img[y][x].green = color_buffer[i*4+1];
+          img[y][x].blue = color_buffer[i*4+2];
+          img[y][x].alpha = color_buffer[i*4+3];
+          
+          // pos_buffer.erase(pos_buffer.begin(), pos_buffer.begin() + 2);
+          // color_buffer.erase(color_buffer.begin(), color_buffer.begin() + 4);
+        } 
+        break;}
+      default:
+        printf("Unknown action: %s\n", name.c_str());
     }
   }
-  /* ... */
-  auto filename = "testout.png";
+
+
   img.save(filename);
-  printf("Image saved with dims %d by %d\n", img.width(), img.height());
+  printf("Image saved with dims %d by %d to output %s \n", img.width(), img.height(), filename);
 }
 
