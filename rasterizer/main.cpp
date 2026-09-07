778478184bce0a5a548942ab6c650f2df5d36dc2
Owen Strong <owensanstrong@gmail.com>
2026-09-06 22:46:28 -0500

    Move pixel push to dedicated context method

--- rasterizer/main.cpp
+++ rasterizer/main.cpp
@@ -14,20 +14,8 @@ using Int8Vec = std::vector<uint8_t>;
 using IntVec = std::vector<int>;
 using FloatVec = std::vector<double>;
 
-int main(int argc, char* argv[]) {
-
-  if (argc < 2) {
-    // No path argument
-    return 1;
-  }
-
-
-  char* filepath = argv[1];
-  StringVec raw_commands = get_lines(filepath);
-  CommandVec commands = get_commands(filepath);
-
-
-
+struct Context {
+  
   // Position / color buffers
   Int8Vec color_buffer; //linear 0-1, not sRGB
   int color_size; // Either 3 or 4
@@ -47,22 +35,45 @@ int main(int argc, char* argv[]) {
   bool frustum_clipping = false; 
 
   // Uniforms
-  Command imgdef = commands.front(); // Assume first command is initializing the image 
-  Image img = Image(std::stoi(imgdef[1]), std::stoi(imgdef[2]));
-  auto filename = imgdef[3].c_str();
-  commands.erase(commands.begin());
-  
-  
+  std::string filename;
+  Image img;
+
+  // Constructor 
+  Context(int width, int height) : img(Image(width, height)) {}
+
+  // Methods
+  void push_pixel(int x, int y, const pixel_t& color) {
+    img[y][x] = color;
+  }
+};
+
+int main(int argc, char* argv[]) {
+
+  if (argc < 2) {
+    // No path argument
+    return 1;
+  }
 
 
-  for (int i = 0; i < commands.size(); i++) {
+  char* filepath = argv[1];
+  StringVec raw_commands = get_lines(filepath);
+  CommandVec commands = get_commands(filepath);
+
+
+  // Read output info
+  Command imgdef = commands.front(); // Assume first command is initializing the image
+  Context gl{std::stoi(imgdef[1]), std::stoi(imgdef[2])};
+  gl.filename = imgdef[3];
+
+
+  for (int i = 1; i < commands.size(); i++) {
     Command cmd = commands[i];
     std::string raw_command = raw_commands[i];
 
 
     // printf("Running command '%s' with %lu args\n", cmd[0].c_str(), cmd.size()-1);
     // printf("Running command: '%s'\n", raw_command.c_str());
-    std::cout << raw_command << std::endl << "==========" << std::endl;
+    std::cout << "==========" << std::endl << raw_command << std::endl;
     
     std::string name = cmd[0];
 
@@ -71,43 +82,23 @@ int main(int argc, char* argv[]) {
         std::cerr << "command 'png' should only be used at start of file";
         break;}
       case "position"_hash:{
-        pos_buffer.clear();
-        for (int i = 2; i < cmd.size(); i++) pos_buffer.push_back(std::stoi(cmd[i]));
-        // for (int i = 2; i < cmd.size(); i+=2) {
-        //   pos_buffer.push_back(std::stoi(cmd[i]));
-        //   printf("Pushing back from x index %d\n", i);
-        //   pos_buffer.push_back(std::stoi(cmd[i+1]));
-        //   printf("Pushing back from y index %d\n", i+1);
-        // }
+        gl.pos_buffer.clear();
+        for (int i = 2; i < cmd.size(); i++) gl.pos_buffer.push_back(std::stoi(cmd[i]));
         break;}
       case "color"_hash:{
-        color_buffer.clear();
-        for (int i = 2; i < cmd.size(); i++) color_buffer.push_back(std::stoi(cmd[i]));
-        // for (int i = 2; i < cmd.size(); i+=4) {
-        //   printf("Pushing back from index %d\n", i);
-        //   color_buffer.push_back(std::stoi(cmd[i]));
-        //   printf("Pushing back from index %d\n", i+1);
-        //   color_buffer.push_back(std::stoi(cmd[i+1]));
-        //   printf("Pushing back from index %d\n", i+2);
-        //   color_buffer.push_back(std::stoi(cmd[i+2]));
-        //   printf("Pushing back from index %d\n", i+3);
-        //   color_buffer.push_back(std::stoi(cmd[i+3]));
-        //   printf("Pushed back from index %d to %d\n", i, i+3);
-        // }
+        gl.color_buffer.clear();
+        for (int i = 2; i < cmd.size(); i++) gl.color_buffer.push_back(std::stoi(cmd[i]));
         break;}
       case "drawPixels"_hash:{
-        printf("Running draw pixel command");
+        printf("Running draw pixel command with buffers of size %lu, %lu into image of dims %d, %d\n", gl.pos_buffer.size(), gl.color_buffer.size(), gl.img.width(), gl.img.height());
         int num_pixels = std::stoi(cmd[1]);
         for (int i = 0; i < num_pixels; i++) {
           printf("Pushing pixel %d of %d\n", i, num_pixels);
-          int x = pos_buffer[i*2+0], y = pos_buffer[i*2+1];
-          img[y][x].red = color_buffer[i*4+0];
-          img[y][x].green = color_buffer[i*4+1];
-          img[y][x].blue = color_buffer[i*4+2];
-          img[y][x].alpha = color_buffer[i*4+3];
-          
-          // pos_buffer.erase(pos_buffer.begin(), pos_buffer.begin() + 2);
-          // color_buffer.erase(color_buffer.begin(), color_buffer.begin() + 4);
+          int x = gl.pos_buffer[i*2+0], y = gl.pos_buffer[i*2+1];
+          // Image& img = gl.img;
+          Int8Vec& cb = gl.color_buffer;
+          int i4 = i*4;
+          gl.push_pixel(x, y, {cb[i4], cb[i4+1], cb[i4+2], cb[i4+3]});
         } 
         break;}
       default:
@@ -116,7 +107,7 @@ int main(int argc, char* argv[]) {
   }
 
 
-  img.save(filename);
-  printf("Image saved with dims %d by %d to output %s \n", img.width(), img.height(), filename);
+  gl.img.save(gl.filename.c_str());
+  printf("Image saved with dims %d by %d to output %s \n", gl.img.width(), gl.img.height(), gl.filename.c_str());
 }
 
